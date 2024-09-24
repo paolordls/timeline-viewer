b273a21929ea336f09fd99c840ce141f2fedc96c
John Achapero <jcachapero@up.edu.ph>
2024-09-24 23:25:45 +0800

    feat(login): added mastodon login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+    export let data;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a class="btn" target="_blank" href="{data.mastodonLoginLink}">Click here to log into a Mastodon Account</a>
