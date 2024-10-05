13463f302dda93a1fb0c09df2e339351fdd551bb
jproads <jp.roads03@gmail.com>
2024-10-05 12:19:49 +0800

    feat: Add initial shadcn and tailwind

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { Input } from "$lib/components/ui/input";
+</script>
+
+
+<div class="flex flex-col items-center justify-center">
+    <Input />
+    <Input />
+</div>
