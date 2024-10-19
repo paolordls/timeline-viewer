d83f299c02db8ef592469475b3540979bf522a40
adzmco23 <112993167+adzmco23@users.noreply.github.com>
2024-10-19 19:31:10 +0800

    feat: add temp UI for displaying timeline data

--- src/routes/timeline/+page.svelte
+++ src/routes/timeline/+page.svelte
@@ -8,3 +8,17 @@
 <p>Username: {status.account.username}</p>
 <p>Post content: {status.content || status.reblog.content}</p>
 {/each}
+
+{#each data.blueskyTimeline as status}
+<h1>post</h1>
+    <p>platform: {status.platform} </p>
+    <p>posterDisplayName: {status.posterDisplayName}</p>
+    <p>posterUsername: {status.posterUsername}</p>
+    <p>postDateTime: {status.postDateTime}</p>
+    <p>postText: {status.postText}</p>
+    <p>postEmbeds: {status.postEmbeds}</p>
+    <p>likes:{status.postEngagement.likes}</p>
+    <p>shares:{status.postEngagement.shares}</p>
+    <p>comments:{status.postEngagement.comments}</p>
+    <p>originalPostLink: {status.originalPostLink}</p>
+{/each}
