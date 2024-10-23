0f3847f8269ec0a055677ef67e7fe4b575d26be6
Jaren Perez <paperez2@up.edu.ph>
2024-10-23 23:02:24 +0800

    fix: Changed connect frontend

--- src/lib/components/ui/navbar.svelte
+++ src/lib/components/ui/navbar.svelte
@@ -1,7 +1,24 @@
+<script lang="ts">
+    import * as Avatar from "$lib/components/ui/avatar";
+    import { Button } from "$lib/components/ui/button";
+    import { RefreshCw } from 'lucide-svelte';
+  </script>
+
 <nav class="bg-inherit text-white p-4 border-b-2 border-solid border-gray-200 max-w-screen">
     <div class="flex flex-row items-center place-content-between">
-        <img src="/pfp.svg" alt="Profile" class="w-10 h-10" />
-        <img src="/kaleido-logomark.svg" alt="Icon" class="w-10 h-10" />
-        <!-- <img src="/refresh.svg" alt="Refresh" class="w-6 h-6" /> -->
+        <!-- User -->
+        <Button class="w-10 h-10 rounded-full bg-transparent hover:bg-transparent">
+            <Avatar.Root>
+                <Avatar.Fallback class="bg-kaleido border-2 border-transparent hover:bg-transparent hover:border-kaleido hover:text-kaleido">RF</Avatar.Fallback>
+            </Avatar.Root>
+        </Button>
+        
+        <!-- Kaleido Logo -->
+        <img src="/kaleido-logomark.svg" alt="Kaleido" class="w-12 h-12" />
+        
+        <!-- Refresh -->
+        <Button class="bg-kaleido w-10 h-10 rounded-full p-2 border-2 border-transparent hover:bg-transparent hover:border-kaleido hover:text-kaleido">
+            <RefreshCw class="w-full h-full" strokeWidth={2}/>
+        </Button>
     </div>
 </nav>
---
+++ src/routes/connect/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    import { Input } from "$lib/components/ui/input";
+    import { Button } from "$lib/components/ui/button";
+    import { Tabs, TabsContent, TabsList, TabsTrigger } from "$lib/components/ui/tabs"
+</script>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<div class="flex flex-cols w-screen h-screen justify-center items-center bg-gradient-to-b from-bluesky to-mastodon">
+    <div class="w-2/3 h-fit bg-white self-center justify-center content-center space-y-6 py-12 rounded-lg shadow-xl">
+        <!-- Kaleido Logo -->
+        <div class="flex flex-col items-center space-y-4">
+            <img src="/kaleido-logomark.svg" alt="Kaleido" class="h-24" />
+            <span class="text-center font-medium block">Connect Account</span>
+        </div>
+        
+        <!-- Form -->
+        <Tabs class="flex flex-col items-center justify-center gap-y-2 px-8 w-full">
+            <!-- Tabs -->
+            <TabsList class="md:w-1/2 w-full bg-white">
+                <TabsTrigger value="mastodon" class="w-1/2 border-2 border-transparent data-[state=active]:border-kaleido">
+                    <img src="/mastodon-logo.svg" alt="Mastodon" class="h-6" />
+                </TabsTrigger>
+                <TabsTrigger value="bluesky" class="w-1/2 border-2 border-transparent data-[state=active]:border-kaleido">
+                    <img src="/bluesky-logo.svg" alt="Bluesky" class="h-6" />
+                </TabsTrigger>
+            </TabsList>
+            
+            <!-- Mastodon -->
+            <TabsContent value="mastodon" class="w-full">
+                <div class="flex flex-col items-center justify-center gap-y-2 w-full">
+                    <Input type="url" placeholder="URL" class="max-w-xs"/>
+                    <Button class="max-w-xs w-full bg-mastodon border-2 border-transparent hover:border-mastodon hover:bg-transparent hover:text-mastodon" href="/login/success">
+                        Connect
+                    </Button>
+                </div>    
+            </TabsContent>
+            
+            <!-- Bluesky -->
+            <TabsContent value="bluesky" class="w-full">
+                <div class="flex flex-col items-center justify-center gap-y-2 w-full">
+                    <Input type="email" placeholder="Email" class="max-w-xs"/>
+                    <Input type="password" placeholder="Password" class="max-w-xs"/>
+                    <Button class="max-w-xs w-full bg-bluesky border-2 border-transparent hover:border-bluesky hover:bg-transparent hover:text-bluesky" href="/login/success">
+                        Connect
+                    </Button>
+                </div>
+            </TabsContent>
+        </Tabs>
+    </div>
+</div>
