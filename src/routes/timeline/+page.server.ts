296e7269b15dd3f1b1604e7b865748d9c352aac8
adzmco23 <112993167+adzmco23@users.noreply.github.com>
2024-11-17 19:40:20 +0800

    fix: refresh bluesky tokens on expiry

--- src/routes/timeline/+page.server.ts
+++ src/routes/timeline/+page.server.ts
@@ -1,7 +1,19 @@
 import type { PageServerLoad } from "../$types";
 import { refreshTimeline } from "./timelineData";
+import { refreshBskyToken } from '$lib/bluesky'
 
 export const load: PageServerLoad = async ({ cookies }) => {
+    // check and refresh bsky token before requesting data
+    if (cookies.get("bskyToken") && cookies.get("bskyDid")) {
+        const bskyTokens = await refreshBskyToken(cookies.get("bskyToken"), cookies.get("bskyRefreshToken"))
+        if (bskyTokens.status === "refreshed"){
+            cookies.set("bskyToken", bskyTokens.token, { path: "/", httpOnly: true, secure: true, maxAge: 172800 })
+            cookies.set("bskyRefreshToken", bskyTokens.refreshToken, { path: "/", httpOnly: true, secure: true, maxAge: 172800 })
+        } 
+
+        console.log(`token status: ${bskyTokens.status}`)
+    }
+
     return {
         timelineData: await refreshTimeline(cookies.get("mastodonToken"), cookies.get("mastodonInstance"), cookies.get("bskyToken"))
     }
